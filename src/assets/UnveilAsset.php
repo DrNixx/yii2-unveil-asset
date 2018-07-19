af3903c7a870ff511aac4c84b62c0a8c474b3b02
Nick Panteleeff <drnix@chess-online.com>
2018-07-19 20:58:25 +0300

    Fix namespace conflict

--- src/assets/UnveilAsset.php
+++ src/assets/UnveilAsset.php
@@ -1,9 +1,9 @@
 <?php
 namespace onix\assets;
 
-use yii\web\AssetBundle;
+use yii\web\AssetBundle as YiiAssetBundle;
 
-class UnveilAsset extends AssetBundle
+class UnveilAsset extends YiiAssetBundle
 {
     public $sourcePath = '@bower/jquery-unveil';
 
